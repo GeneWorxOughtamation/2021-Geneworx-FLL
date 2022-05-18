2e5f5df41a42fa191c19143c788f111f073342ad
santino <sntntomasi@gmail.com>
2022-05-17 20:26:19 -0400

    Backing up files.

--- users/Santino/all arrays/array_follower/array_gyro_arrays.py
+++ users/Santino/all arrays/array_follower/array_gyro_arrays.py
@@ -7,7 +7,7 @@
 
 #Uses zero distance if you want to turn without moving.
 
-#                 1  2  3  4  5  6  7  8  9 10 11
-distance_array= [   0,  0]
-speed_array=    [  60, 60]
-angle_array=    [ -30, 90]
+#                   1  2  3  4   5   6  7  8  9 10 11
+distance_array= [   2, 0,30, 0, 55, 64]
+speed_array=    [  60,60,90,60,120,120]
+angle_array=    [   0,55,55, 3,  3, 33]
