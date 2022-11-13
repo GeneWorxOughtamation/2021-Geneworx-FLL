70305b2d957bfe68a3c636f1f85bee1083bf435e
D9C413 <pg415253@gmail.com>
2022-11-13 15:55:05 -0500

    This is Peiro's up and down elevator.

---
+++ users/Edson/Edson/learning/main.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env pybricks-micropython
+def I2mm( inches ):
+    mm = 25.4 * inches
+    return mm
+
+"""
+Example LEGO® MINDSTORMS® EV3 Robot Educator Driving Base Program
+-----------------------------------------------------------------
+
+This program requires LEGO® EV3 MicroPython v2.0.
+Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
+
+Building instructions can be found at:
+https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, GyroSensor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+
+
+
+
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B,positive_direction=Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.C,positive_direction=Direction.COUNTERCLOCKWISE)
+Lmotor_updown = Motor(Port.A,positive_direction=Direction.COUNTERCLOCKWISE)
+Medmotor_leftright = Motor(Port.D,positive_direction=Direction.COUNTERCLOCKWISE)
+gyro = GyroSensor(Port.S4,Direction.CLOCKWISE)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+# UD = up,down
+# LR = left,right
+
+def gyrom(dis, ang, speed):
+    gyro.reset_angle(0)
+    kp = 3
+    #every time the "gost" function is used all the three variables will be swapped out with
+    #  what's in the paranthesis in the order shown above.
+    if I2mm(dis) >=0 : 
+        while robot.distance() <= I2mm(dis):
+            print( robot.distance() )     
+            correction = (ang - gyro.angle())*kp
+            robot.drive(speed,correction)
+            #Robot will stop and the large motor's distance will be reset.
+            #The angle of the gyro sensor will also be reset back to zero.
+        robot.stop()
+        left_motor.brake()
+        right_motor.brake()
+        print( robot.distance() )
+        left_motor.reset_angle(0)
+        right_motor.reset_angle(0)
+
+    else :
+        while robot.distance() >= I2mm(dis):    
+            correction = (ang - gyro.angle())*kp
+            robot.drive(-speed,correction)
+            #Robot will stop and the large motor's distance will be reset.
+            #The angle of the gyro sensor will also be reset back to zero.
+        robot.stop()
+        left_motor.brake()
+        right_motor.brake()
+        print( robot.distance() )
+        left_motor.reset_angle(0)
+        right_motor.reset_angle(0)
+
+def eUPmovement(speed, UP ):
+    if UP >= 0:
+        while Lmotor_updown.angle() <= UP:
+            Lmotor_updown.run_target(speed, UP)
+            if Lmotor_updown.angle() >= UP:
+                break
+
+    elif UP <= 0:
+        while Lmotor_updown.angle() >= UP:
+            Lmotor_updown.run_target(speed, UP)
+            if Lmotor_updown.angle() <= UP:
+                break
+
+            
+
+
+gyrom(11, 0, 150)
+eUPmovement(300, 100)
+gyrom(-12,0, 200)
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                    
+
+
